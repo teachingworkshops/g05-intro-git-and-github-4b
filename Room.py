714dad470817606a4ca71ad4a4deaa8ecedcafcc
kongshijatwit <kongshij@wit.edu>
2024-01-28 14:35:43 -0500

    Modified room variables, must have names

--- Room.py
+++ Room.py
@@ -1,29 +1,34 @@
 class Room:
-    _connected_rooms = {}  # Assume (String direction: Room room) pair
+    _connected_rooms = {}  # Assume (String name: Room room) pair
+    _name = ""
     _description = ""
     _has_key = False
     _is_exit = False
+    _is_cleared = False
 
-    def __init__(self, description):
+    def __init__(self, name, description):
         """
         Initializes a new room object
 
         Args:
+            name (String): Name of the current room
             description (String): Some words telling the player about the current room
         """
         self._description = description
+        self._name = name
         self._connected_rooms = {}
         self._has_key = False
         self._is_exit = False
 
-    def add_room(self, name):
+    @property
+    def connected_rooms(self):
         """
-        Adds a room to the current room
+        Returns the dictionary of rooms connected to the current room
 
-        Args:
-            name (String): Name of the new room
+        Returns:
+            dict: connected rooms dictionary
         """
-        self._connected_rooms[name] = Room("")
+        return self._connected_rooms
 
     @property
     def get_room(self, name):
@@ -39,21 +44,24 @@ class Room:
         return self._connected_rooms[name]
 
     @property
-    def get_description(self):
+    def description(self):
         """
         Returns the description of the current room
 
         Returns:
-            String: Room description
+            String: _description
         """
         return self._description
 
-    def get_directions(self):
+    @property
+    def connected_names(self):
         """
-        Prints the all room names connected to the current room
+        Returns the names of all rooms connected to the current room
+
+        Returns:
+            list: All connected room names
         """
-        for key in self._connected_rooms:
-            print(key)
+        return list(self._connected_rooms.keys())
 
     def set_has_key(self, new_value):
         """
@@ -62,12 +70,12 @@ class Room:
         self._has_key = new_value
 
     @property
-    def get_has_key(self):
+    def has_key(self):
         """
         Returns whether the current room has a key or not
 
         Returns:
-            Boolean: Key
+            Boolean: _has_key
         """
         return self._has_key
     
@@ -78,11 +86,43 @@ class Room:
         self._is_exit = new_value
     
     @property
-    def get_exit(self):
+    def is_exit(self):
         """
         Returns whether the room is an exit
 
         Return:
-            Boolean: Exit
+            Boolean: _is_exit
         """
         return self._is_exit
+    
+    def set_cleared(self, new_value):
+        """
+        Sets the _is_cleared flag
+        """
+        self._is_cleared = new_value
+
+    @property
+    def is_cleared(self):
+        """
+        Gets whether a room is cleared of enemies
+
+        Returns:
+            Boolean: _is_cleared
+        """
+        return self._is_cleared
+    
+    def set_name(self, new_value):
+        """
+        Sets the name of the current room
+        """
+        self._name = new_value
+
+    @property
+    def name(self):
+        """
+        Gets the name of the current room
+
+        Returns:
+            String: Name of current room
+        """
+        return self._name

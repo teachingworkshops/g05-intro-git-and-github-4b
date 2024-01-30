9ab6ac35b7362724764e5c323765db15322cc2a9
briansanchez1 <115597244+briansanchez1@users.noreply.github.com>
2024-01-29 22:54:21 -0500

    Added main.py
    This is not the final and is still missing most of the fixes that we talked about, just pushing so that we all have something to look at and reference. If there is anything that you want to change, feel free to

---
+++ main.py
@@ -0,0 +1,76 @@
+from enemy import Enemy
+from assets import Assets
+from level_builder import build_level
+
+
+class Main:
+    def __init__(self):
+        self.current_level = build_level()
+        self.has_key = False
+        self.game_over = False
+        self.difficulty_counter = 0
+        
+    def intro(self): 
+        print(Assets.GAME_NAME)
+        print(Assets.INTRO_MSG)
+        
+    def prompt_user(self):
+        print(f"What room would you like to move to?\nYou are currently in {self.current_level.name}. You can travel to...")
+        for connected_name in self.current_level.connected_names:
+            print('\t' + connected_name)
+        response = input("Choose wisely: ")
+        self.check_response(response)
+        
+    def check_response(self, response):
+        
+        while response not in self.current_level.connected_rooms:
+            print("\nNot quite. Please enter a valid connected room name.")
+            print(f"You can only travel to...")
+            for connected_name in self.current_level.connected_names:
+                print('\t' + connected_name)
+            response = input("choose wisely: ")
+        self.move_user(response)
+        
+    def move_user(self, response):
+        self.current_level = self.current_level.connected_rooms[response]
+        print(f"Good Job, you have successfully moved to {self.current_level._name}!")
+        print(self.current_level.description)
+        print()
+        
+    def check_game_over(self):
+        if self.has_key and self.current_level.is_exit: #fix this (check boss first)
+            response = input('Would you like to restart? Enter Y or N: ') # add check for invalid response later
+            if response.lower() == 'y':
+                self.restart_game()
+            else:
+                self.game_over = True
+        
+    def check_has_key(self):
+        if(self.current_level.has_key):
+                print("Congrats you got the key\n")
+                self.has_key = True
+    
+    def restart_game(self):
+        game = Main()
+        game.play_game()
+        
+        
+    # def math_prompt():
+    
+    # def credits():
+        
+        
+    def play_game(self):
+        self.intro()
+        while not self.game_over:
+            self.prompt_user()
+            self.check_has_key()
+            self.check_game_over()
+            # boss and math problems
+
+    
+
+if __name__ == "__main__":
+    
+    game = Main()
+    game.play_game()

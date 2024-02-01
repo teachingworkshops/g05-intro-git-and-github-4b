d8f020f4676373c01bc8f06e75a494c677e50d92
Nehummus <90570220+Nehummus@users.noreply.github.com>
2024-01-31 21:05:31 -0500

    Updated the logic and included proper comments

--- math_question.py
+++ math_question.py
@@ -2,21 +2,38 @@ import random
 from assets import Assets
 
 class MathQuestion:
-
+    """
+        Gives out a math question
+    """
     # Dictionary keeping track of the used questions for each difficulty
     used_questions = {0: set(), 1: set(), 2: set()}
 
     @staticmethod
     def generate_question(difficulty):
+        """
+        Generates a question based on the alloted difficulty.
+        Difficulty values with correspondence:
+        0 = boss
+        1-6 = easy
+        7-12 = medium
+        13+ = hard
 
+        Args: 
+            difficulty (int): Incremented in order to change the difficulty of the questions.
+
+        Returns:
+            string array containing question, correct_answer
+            
+        """
+        
         # Checks for difficulty and chooses set accordingly
-        if difficulty == 0:
+        if difficulty >= 1 and difficulty <= 6:
             available_questions = set(Assets.easy_questions)
-        elif difficulty == 1:
+        elif difficulty >= 7 and difficulty <= 12:
             available_questions = set(Assets.medium_questions)
-        elif difficulty == 2:
+        elif difficulty >= 13:
             available_questions = set(Assets.hard_questions)
-        elif difficulty == 3:
+        elif difficulty == 0:
             available_questions = set(Assets.boss_questions)
 
         # Makes set with only unused questions

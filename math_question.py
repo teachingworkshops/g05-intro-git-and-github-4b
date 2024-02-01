c26ecbea7c9bd781365571fb8a6df24d59cc67e4
Nehummus <90570220+Nehummus@users.noreply.github.com>
2024-01-31 22:01:50 -0500

    Got rid of check and hopefully fixed the logic (please im so done)

--- math_question.py
+++ math_question.py
@@ -5,8 +5,6 @@ class MathQuestion:
     """
         Gives out a math question
     """
-    # Dictionary keeping track of the used questions for each difficulty
-    used_questions = {0: set(), 1: set(), 2: set(), 3: set()"}
 
     @staticmethod
     def generate_question(difficulty):
@@ -28,22 +26,12 @@ class MathQuestion:
         
         # Checks for difficulty and chooses set accordingly
         if 1 <= difficulty <= 6:
-            available_questions = set(Assets.easy_questions)
+            question, correct_answer = random.choice(Assets.easy_questions)
         elif 7 <= difficulty <= 12:
-            available_questions = set(Assets.medium_questions)
+            question, correct_answer = random.choice(Assets.medium_questions)
         elif difficulty >= 13:
-            available_questions = set(Assets.hard_questions)
+            question, correct_answer = random.choice(Assets.hard_questions)
         elif difficulty == 0:
-            available_questions = set(Assets.boss_questions)
-
-        # Makes set with only unused questions
-        unused_questions = available_questions - MathQuestion.used_questions[difficulty]
-
-        # Chooses random question from unused questions list
-        question, correct_answer = random.choice(list(unused_questions))
-
-        # Marks questions as answered
-        MathQuestion.used_questions[difficulty].add((question, correct_answer))
+            question, correct_answer = random.choice(Assets.boss_questions)
 
         return question, correct_answer
-
